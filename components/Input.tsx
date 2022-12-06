f34082c6c1b6b7c8d0eebb8f9376cb0979db748e
Ornella Radica <radicaornella@gmail.com>
2022-12-06 14:34:00 +0100

    Add validations

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,14 +1,26 @@
-import { Field } from "formik";
+import { Field, useFormikContext } from "formik";
 
 interface Props {
   label: JSX.Element | string;
   name: string;
   type: "text" | "number";
   placeholder: string;
+  maxLength?: number;
+  mask?: (value: any) => string;
 }
 
-const Input: React.FC<Props> = ({ label, name, type, placeholder }) => {
+const Input: React.FC<Props> = ({
+  label,
+  name,
+  type,
+  placeholder,
+  maxLength,
+  mask,
+}) => {
   const error = "";
+  const { getFieldProps } = useFormikContext();
+  const { value } = getFieldProps(name);
+
   return (
     <div className="flex flex-col gap-2 w-full">
       <label htmlFor={name} className="capitalize">
@@ -19,6 +31,8 @@ const Input: React.FC<Props> = ({ label, name, type, placeholder }) => {
         name={name}
         type={type}
         placeholder={placeholder}
+        maxLength={maxLength}
+        value={mask ? mask(value) : value}
         className="placeholder-input p-3 border border-input rounded-lg"
       />
       {error && <span>{error} </span>}
--- components/PaymentForm.tsx
+++ components/PaymentForm.tsx
@@ -1,9 +1,27 @@
+import * as yup from "yup";
 import { PaymentIcon, LockIcon } from "./Icons";
 import Button from "./Button";
 import { Form, Formik } from "formik";
 import Input from "./Input";
 
+const initialValues = {
+  full_name: "",
+  card_number: "",
+  expiration: "",
+  cvv: "",
+  zip_code: "",
+};
 function PaymentForm() {
+  const validateSchema = yup.object().shape({
+    full_name: yup.string().required("Input is required"),
+    card_number: yup
+      .string()
+      .required("Input is required")
+      .min(16, "not less than 16"),
+    expiration: yup.string().required("Input is required").min(5),
+    cvv: yup.string().required("Input is required").max(3).min(3),
+    zip_code: yup.string().required("Input is required"),
+  });
   return (
     <div className="flex flex-col gap-6 justify-center items-center bg-white w-[319px] rounded-lg p-8 sm:shadow-lg sm:border sm:border-gray-50">
       <div className="flex flex-col justify-center items-center gap-2">
@@ -14,7 +32,8 @@ function PaymentForm() {
         onSubmit={(values) => {
           console.log(values);
         }}
-        initialValues={{ full_name: "" }}
+        initialValues={initialValues}
+        validationSchema={validateSchema}
       >
         <Form className="w-full flex flex-col gap-6 justify-center items-center">
           <Input
@@ -22,12 +41,22 @@ function PaymentForm() {
             name="full_name"
             type="text"
             placeholder="Your Name"
+            mask={(value) => value && value.replace(/[^a-zA-Z\s]+/g, "")}
           />
           <Input
             label="Card number"
             name="card_number"
-            type="number"
             placeholder="1234 1234 1234 1234"
+            type="text"
+            maxLength={19}
+            mask={(value) =>
+              value &&
+              value
+                .replace(/\s/g, "")
+                .replace(/[^0-9]+/g, "")
+                .replace(/(\d{4})/g, "$1 ")
+                .trim()
+            }
           />
           <div className="grid grid-cols-2 gap-6">
             <Input
@@ -35,13 +64,49 @@ function PaymentForm() {
               name="expiration"
               type="text"
               placeholder="MM/YY"
+              maxLength={5}
+              mask={(value) => {
+                if (value === "/") return "";
+                if (value.includes("/")) {
+                  const chars = value.split("");
+                  // Search for / if its repeated more than once, remove the last one.
+                  if (chars.filter((c: string) => c === "/").length > 1) {
+                    // Remove last repetition of "/" and join the characters
+                    chars.pop();
+                    return chars.join("");
+                  }
+                }
+                if (value.length === 2) {
+                  if (parseFloat(value) > 12) {
+                    const [first, second] = value.split("");
+                    return "0" + first + "/" + second;
+                  } else if (value.includes("/")) {
+                    const [first] = value.split("");
+                    return "0" + first + "/";
+                  }
+                }
+                if (value.length === 3 && !value.includes("/")) {
+                  const [first, second, third] = value.split("");
+                  return first + second + "/" + third;
+                }
+                return value.replace(/[^0-9/]+/g, "");
+              }}
             />
-            <Input label="CVV" name="cvv" type="number" placeholder="..." />
+            <Input
+              label="CVV"
+              name="cvv"
+              type="text"
+              placeholder="..."
+              maxLength={3}
+              mask={(value) =>
+                value && value.replace(/\s/g, "").replace(/[^0-9]+/g, "")
+              }
+            />
           </div>
           <Input
             label="Zip code"
             name="zip_code"
-            type="number"
+            type="text"
             placeholder="Your zip"
           />
 
